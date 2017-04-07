a3c260e8c20e691394d90ba973463c12e0c011fa
Kelly Marton <kellymmarton@gmail.com>
2017-04-07 16:11:33 -0400

    add media query and layout for tablet view

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $('#button1').click(function () {
+        $('#hide1').toggle('slow');
+    });
+
+    $('#button2').click(function () {
+        $('#hide2').toggle('slow');
+    });
+
+    $('#button3').click(function () {
+        $('#hide3').toggle('slow');
+    });
+
+    $('#button4').click(function () {
+        $('#hide4').toggle('slow');
+    });
+
+});
